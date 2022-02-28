42db81418b376dea355c31f8a924acadec8eae87
Juan Emiliano Galván Chaire <41383107+EmilianoGCH@users.noreply.github.com>
2022-02-28 10:40:57 -0600

    Update Scrapy only springer
    There are duplicate in this script so I need to erase one of them

--- additive_review/spiders/scrapy_additive.py
+++ additive_review/spiders/scrapy_additive.py
@@ -26,32 +26,3 @@ class additive_review(scrapy.Spider):
         item["price"] = response.selector.xpath('').getall()
         yield Item
 
-
-
-
-import scrapy
-from guadalajararealstate.items import guadalajararealstate.Item
-
-class ReasState(scrapy, Spider):
-    name = 'realstate'
-
-    def start_request(self):
-        number_of_pages = 3
-        urls = []
-        root_url = 'https://link.springer.com/search/page/'
-        #"https://link.springer.com/search/page/1?facet-content-type=%22Article%22&query=additive+manufacturing"
-        url_sufix = "?facet-content-type=%22Article%22&query=additive+manufacturing"
-        for page in range(1,number_of_pages+1):
-            urls.append(root_url+str(page)+url_sufix)
-
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.obtain_adresses)
-
-    def obtain_adresses(self,response):
-        page_adresses = response.selector.xpath("//*[@id="results-list"]/li[1]/h2/a/@href")
-        for page_url in page_adresses:
-            yield scrapy.Request(url=""+page_url,callback=self.get_property_information)
-
-    def get_property_information(self, response):
-        item['title'] = response.selector.xpath('//*[@id="main-content"]/div/div/article/div/div[1]/div[2]/h1/text()').get()
-        yield item
